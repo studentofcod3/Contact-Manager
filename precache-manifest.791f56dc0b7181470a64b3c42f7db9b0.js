99a62d53b944a1e12f7ca3084f65be2ccf51859a
ayyoub <ayyoub@hotmail.com>
2019-06-06 23:44:16 +0200

    Updates

---
+++ precache-manifest.791f56dc0b7181470a64b3c42f7db9b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dde90cb38ee33ceb40136e483bf2147",
+    "url": "/Contact_Manager/index.html"
+  },
+  {
+    "revision": "0e7bfc43727a69141c3a",
+    "url": "/Contact_Manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0e7bfc43727a69141c3a",
+    "url": "/Contact_Manager/static/js/2.071b9d97.chunk.js"
+  },
+  {
+    "revision": "aa88a275614416f701f9",
+    "url": "/Contact_Manager/static/js/main.95c34cc6.chunk.js"
+  },
+  {
+    "revision": "eaf18a0ba7eeb5677e43",
+    "url": "/Contact_Manager/static/js/runtime~main.5ea2a720.js"
+  }
+]);
